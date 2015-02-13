287879edee82ea92b96f22040923b5349046c092
Bacher <bacherko@gmail.com>
2015-02-13 14:31:34 +0300

    master-slave arch

--- convert-runner/main.js
+++ convert-runner/main.js
@@ -1,50 +1,59 @@
 
-const CHECK_DELAY = 2000;
-const UPLOAD_PATH = '/Users/Bacher/tmp/movie-upload';
-const PUBLIC_PATH = '/Users/Bacher/tmp/movie-www';
+function start() {
 
-process.chdir(PUBLIC_PATH);
+    const CHECK_DELAY = 2000;
+    const UPLOAD_PATH = '/Users/Bacher/tmp/movie-upload';
+    const PUBLIC_PATH = '/Users/Bacher/tmp/movie-www';
 
-var fs = require('fs');
-var Path = require('path');
+    process.chdir(PUBLIC_PATH);
 
-var FolderAnalyser = require('./folder-analyser');
-var DBVideo = require('./db-video');
-var VideoUtils = require('./ffmpeg-runner');
+    var fs = require('fs');
+    var Path = require('path');
 
-var folderAnalyser = null;
+    var FolderAnalyser = require('./folder-analyser');
+    var DBVideo = require('./db-video');
+    var VideoUtils = require('./ffmpeg-runner');
 
-DBVideo.connect({
-    host: 'localhost',
-    database: 'test',
-    table: 'videos'
-}).then(
-    function() {
-        folderAnalyser = new FolderAnalyser();
+    var folderAnalyser = null;
 
-        folderAnalyser.startWatch(UPLOAD_PATH, CHECK_DELAY, onVideoUploaded);
-    },
-    function(err) {
-        console.log(err);
+    DBVideo.connect({
+        host: 'localhost',
+        database: 'test',
+        table: 'videos'
+    }).then(
+        function() {
+            folderAnalyser = new FolderAnalyser();
+
+            folderAnalyser.startWatch(UPLOAD_PATH, CHECK_DELAY, onVideoUploaded);
+        },
+        function(err) {
+            console.log(err);
+        }
+    );
+
+    function onVideoUploaded(fileName) {
+        DBVideo.createNewVideo().then(function(id) {
+            var videoRootPath = Path.join(PUBLIC_PATH, id);
+            var videosPath = Path.join(videoRootPath, 'videos');
+            var imagesPath = Path.join(videoRootPath, 'images');
+
+            var uploadFileName = 'upload' + Path.extname(fileName);
+
+            fs.mkdirSync(videoRootPath);
+            fs.mkdirSync(videosPath);
+            fs.mkdirSync(imagesPath);
+
+            fs.renameSync(Path.join(UPLOAD_PATH, fileName), Path.join(videoRootPath, uploadFileName));
+
+            VideoUtils.startProcess(id, uploadFileName);
+        }).catch(function(e) {
+            console.log('EEEEE', e);
+        });
     }
-);
+}
 
-function onVideoUploaded(fileName) {
-    DBVideo.createNewVideo().then(function(id) {
-        var videoRootPath = Path.join(PUBLIC_PATH, id);
-        var videosPath = Path.join(videoRootPath, 'videos');
-        var imagesPath = Path.join(videoRootPath, 'images');
-
-        var uploadFileName = 'upload' + Path.extname(fileName);
-
-        fs.mkdirSync(videoRootPath);
-        fs.mkdirSync(videosPath);
-        fs.mkdirSync(imagesPath);
-
-        fs.renameSync(Path.join(UPLOAD_PATH, fileName), Path.join(videoRootPath, uploadFileName));
-
-        VideoUtils.startProcess(id, uploadFileName);
-    }).catch(function(e) {
-        console.log('EEEEE', e);
-    });
+if (module.exports) {
+    module.exports = start;
+} else {
+    start();
 }
---
+++ convert-runner/master.js
@@ -0,0 +1,20 @@
+
+var cluster = require('cluster');
+
+if (cluster.isMaster) {
+    startFork();
+
+    cluster.on('exit', function(worker, code) {
+        console.log('Slave process ' + worker.process.pid + ' died, errorcode: ' + code);
+
+        setTimeout(startFork, 1000);
+    });
+} else {
+    require('./main')();
+}
+
+function startFork() {
+    cluster.fork();
+
+    console.log('Slave process started.');
+}
